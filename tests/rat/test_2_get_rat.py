cf6a19944c32ff57d2fb2df8c28765db46071707
VeronicaLil <105051293+VeronicaLil@users.noreply.github.com>
2022-05-11 17:42:03 +0200

    Get a rat test added

--- tests/rat/test_2_get_rat.py
+++ tests/rat/test_2_get_rat.py
@@ -19,7 +19,43 @@ def test_get_rats(request):
     #Request
     with open("tests/test_data.yaml", "r") as yaml_file:
         rat_data = yaml.safe_load(yaml_file)
-        get_response = client.get("/rat", json=rat_data)
+        get_response = client.get("/rat")
+
+        # Save response
+        request.node._effective_status_code = get_response.status_code
+
+        #Verify the result
+        request.node._expected_status_code = 200
+        assert get_response.status_code == 200
+
+        # Save the log
+        request.node._full_response = get_response.status_code
+
+
+
+"""
+#########################################
+#           Get a rat                #
+#########################################
+"""
+
+
+# Get a rat
+def test_get_rat(request):
+    # Configure test info in the report
+    request.node._method = "GET"
+    request.node._route = "/rat"
+    request.node._title = "Test get a rat"
+    request.node._description = "Pass if the status code is 200"
+
+    #Request
+    with open("tests/test_data.yaml", "r") as yaml_file:
+        rat_data = yaml.safe_load(yaml_file)
+        get_all = client.get("/rat")
+        rat_id = get_all.json()[-1]["rat_id"]
+
+        # Get a rat
+        get_response = client.get(f"/rat/{rat_id}")
 
         # Save response
         request.node._effective_status_code = get_response.status_code
